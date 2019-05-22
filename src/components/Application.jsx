046f533cb0f24fcfaa8209444794778b71d200d7
Dan <headhertz88@gmail.com>
2019-05-22 21:33:02 +0100

    usercontext provider and hook

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,28 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { collectPosts } from '../utils';
-import { firestore, auth, createUserProfileDocument } from '../firebase';
+import React from 'react';
 import Posts from './Posts';
 import Authentication from './Authentication';
 
 const Application = () => {
-	const [user, setUser] = useState(null);
-
-	useEffect(() => {
-		const unsubscribeAuth = auth.onAuthStateChanged(async userAuth => {
-			const user = await createUserProfileDocument(userAuth);
-
-			setUser(user);
-		});
-
-		return () => {
-			unsubscribeAuth();
-		};
-	}, []);
-
 	return (
 		<main className="Application">
 			<h1>Think Piece</h1>
-			<Authentication user={user} />
+			<Authentication />
 			<Posts />
 		</main>
 	);
---
+++ src/providers/UserProvider.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, createContext } from 'react';
+import { auth, createUserProfileDocument } from '../firebase';
+
+const UserContext = createContext([{}, () => {}]);
+
+const UserProvider = props => {
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		const unsubscribeAuth = auth.onAuthStateChanged(async userAuth => {
+			const user = await createUserProfileDocument(userAuth);
+
+			setUser(user);
+		});
+
+		return () => {
+			unsubscribeAuth();
+		};
+	}, []);
+
+	return <UserContext.Provider value={[user, setUser]}>{props.children}</UserContext.Provider>;
+};
+
+export { UserContext, UserProvider };
