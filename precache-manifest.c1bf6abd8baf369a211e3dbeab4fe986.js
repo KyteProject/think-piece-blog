9c2ed5b5b4686180516a0deac134222c1e99a294
Daniel Smith <headhertz88@gmail.com>
2019-10-22 15:36:57 +0100

    Updates

---
+++ precache-manifest.c1bf6abd8baf369a211e3dbeab4fe986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f87373d9ade0ab118125f02c0fcbed8",
+    "url": "/think-piece-blog/index.html"
+  },
+  {
+    "revision": "cb3666b9f61645d54ec5",
+    "url": "/think-piece-blog/static/css/main.85cc7bef.chunk.css"
+  },
+  {
+    "revision": "1368e2b4af47bc6068dd",
+    "url": "/think-piece-blog/static/js/2.c37d4440.chunk.js"
+  },
+  {
+    "revision": "cb3666b9f61645d54ec5",
+    "url": "/think-piece-blog/static/js/main.b3c5a38e.chunk.js"
+  },
+  {
+    "revision": "3d0e6f7d28bba9f694ec",
+    "url": "/think-piece-blog/static/js/runtime~main.b06fd2b9.js"
+  }
+]);
